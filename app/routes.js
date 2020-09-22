1e404ea5098032bfd455b4be944baea09855deea
Gaz Aston <spiritbased@gmail.com>
2020-09-22 16:42:45 +0100

    add basic routing based on username
    it’s just a prototype — if we have to duplicate content on different usertype pages for now, that’s fine.

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/authenticate', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const username = req.session.data['user-username']
+
+  if (username === 'mca') {
+    res.redirect('/mca')
+  } else if (username === 'dft') {
+    res.redirect('/dft')
+  }
+})
+
 module.exports = router
