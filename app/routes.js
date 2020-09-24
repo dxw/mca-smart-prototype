324582b9763af5f77ed0b065bdcb4db40e5b248c
Gaz Aston <spiritbased@gmail.com>
2020-09-24 16:18:55 +0100

    remove conditional route for reporting login
    …permature optimisation :)

--- app/routes.js
+++ app/routes.js
@@ -3,18 +3,4 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.post('/authenticate', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  const username = req.session.data['user-username']
-
-  if (username === 'mca') {
-    res.redirect('reporting/dashboard-mca')
-  } else if (username === 'dft') {
-    res.redirect('dashboard-dft')
-  }
-})
-
 module.exports = router
